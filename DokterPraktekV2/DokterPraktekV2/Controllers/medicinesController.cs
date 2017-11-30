df65d6b1e3b850cc30826ccdea2893d95d5b3d0e
Johan <johansuryanto@outlook.com>
2017-11-30 13:59:11 +0700

    Commit cont:doctor,medicine,medTransaction,and the View

---
+++ DokterPraktekV2/DokterPraktekV2/Controllers/medicinesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DokterPraktek;
+using DokterPraktekV2;
+
+namespace DokterPraktek.Controllers
+{
+    public class medicinesController : Controller
+    { 
+        private DokterPraktekEntities1 db = new DokterPraktekEntities1();
+
+        // GET: medicines
+        public ActionResult Index()
+        {
+            var medicine = db.medicines.Include(m => m.doctor);
+            return View(medicine.ToList());
+        }
+
+        // GET: medicines/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            medicine medicine = db.medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // GET: medicines/Create
+        public ActionResult Create(int id)
+        {
+            var namaid = db.doctors.Find(id).name;
+            var idx = db.doctors.Find(id).id;
+            ViewBag.idx = idx;
+            ViewBag.ID = namaid;
+            ViewBag.doctorId = new SelectList(db.doctors, "id", "name");
+            return View();
+        }
+
+        // POST: medicines/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "doctorId,name,price,quantity,dateIn,expired")] medicine medicine,[Bind(Include ="id,medicineId,doctorId,statusTransaction,quantity")] medicineTransaction medtrans)
+        {
+            medicine.dateIn = DateTime.Today;
+            ViewBag.getData = db.medicineTransactions;
+            //if(medtrans.statusTransaction == true)
+            //{
+            //    medtrans.statusTransaction = true;
+            //}else
+            //{
+            //    medtrans.statusTransaction = true;
+            //}
+            if (ModelState.IsValid)
+            {
+                medtrans.statusTransaction = true;
+                db.medicineTransactions.Add(medtrans);
+                db.medicines.Add(medicine);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.doctorId = new SelectList(db.doctors, "id", "name", medicine.doctorId);
+            return View(medicine);
+        }
+
+        // GET: medicines/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            medicine medicine = db.medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.doctorId = new SelectList(db.doctors, "id", "name", medicine.doctorId);
+            return View(medicine);
+        }
+
+        // POST: medicines/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,doctorId,name,harga,quantity,dateIn,expired")] medicine medicine)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicine).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.doctorId = new SelectList(db.doctors, "id", "name", medicine.doctorId);
+            return View(medicine);
+        }
+
+        // GET: medicines/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            medicine medicine = db.medicines.Find(id);
+            if (medicine == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicine);
+        }
+
+        // POST: medicines/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            medicine medicine = db.medicines.Find(id);
+            db.medicines.Remove(medicine);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
