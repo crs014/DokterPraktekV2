a063c37c8f36a1b9cb050554e670fe7f1dff0f23
Johan <johansuryanto@outlook.com>
2017-12-07 17:19:22 +0700

    vm stock,detail index stock for docter medicine

---
+++ DokterPraktekV2/DokterPraktekV2/Models/VM_Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DokterPraktekV2.Models
+{
+    public class VM_Stock
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public Nullable<int> inStock { get; set; }
+        public Nullable<int> outStock { get; set; }
+        public Nullable<int> remainStock { get; set; }
+    }
+}
