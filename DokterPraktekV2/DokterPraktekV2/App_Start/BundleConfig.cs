6a3345327903eb4821b11a09f430f9c80a77605b
cristono14 <cristonowijaya@gmail.com>
2017-12-11 11:12:36 +0700

    add server side medicine list on input history

--- DokterPraktekV2/DokterPraktekV2/App_Start/BundleConfig.cs
+++ DokterPraktekV2/DokterPraktekV2/App_Start/BundleConfig.cs
@@ -9,10 +9,11 @@ namespace DokterPraktekV2
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.2.1.min.js", 
+                        "~/Plugins/DataTables-1.10.16/js/jquery.dataTables.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*", "~/Scripts/datatables.min.js"));
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -26,7 +27,7 @@ namespace DokterPraktekV2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/datatables.min.css"));
+                      "~/Plugins/DataTables-1.10.16/css/jquery.dataTables.min.css"));
         }
     }
 }
