77d79ba47dd3e97e6c719ce2de16d25c745beee8
cristono14 <cristonowijaya@gmail.com>
2017-12-12 16:00:31 +0700

    change dashboard view page

--- DokterPraktekV2/DokterPraktekV2/App_Start/BundleConfig.cs
+++ DokterPraktekV2/DokterPraktekV2/App_Start/BundleConfig.cs
@@ -9,11 +9,11 @@ namespace DokterPraktekV2
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.2.1.min.js", 
-                        "~/Plugins/DataTables-1.10.16/js/jquery.dataTables.min.js"));
+                        "~/Scripts/jquery-3.2.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                         "~/Plugins/DataTables-1.10.16/js/jquery.dataTables.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
