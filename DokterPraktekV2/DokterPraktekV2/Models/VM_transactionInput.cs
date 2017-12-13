b3119360660c638a8059c505fe57ab45de8c0509
cristono14 <cristonowijaya@gmail.com>
2017-12-13 16:02:33 +0700

    add Payment Module

---
+++ DokterPraktekV2/DokterPraktekV2/Models/VM_transactionInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DokterPraktekV2.Models
+{
+    public class VM_transactionInput
+    {
+        [Required]
+        public decimal amount { get; set; }
+    }
+}
